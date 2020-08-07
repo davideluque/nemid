699f9c4729eac429e7cd047a0b4ffe58cf9169a0
David Cabeza <cabezadavide@gmail.com>
2020-08-06 23:42:20 -0400

    Adds Base64 encoded der representation method

--- lib/nemid/crypto.rb
+++ lib/nemid/crypto.rb
@@ -1,12 +1,25 @@
+require 'base64'
+require 'openssl'
+
 module NemID
  class Crypto
+  
   def initialize(certificate, pass)
-    @certificate = certificate
-    @pass = pass
+    certificate = read_file(certificate)
+    @pkcs12 = read_pkcs12(certificate, pass)
   end
 
-  def read_certificate
-    File.read(@certificate)
+  def base64_encoded_der_representation
+    Base64.strict_encode64(@pkcs12.certificate.to_der)
+  end
+
+  private
+  def read_file(certificate)
+    File.read(certificate)
+  end
+
+  def read_pkcs12(certificate, pass)
+    OpenSSL::PKCS12::new(certificate, pass)
   end
  end
 end
