c37c9b2c3163c1bc5b9c53d8a2f4aef79ef2b1d6
David Cabeza <cabezadavide@gmail.com>
2020-08-07 01:25:06 -0400

    Adds base64_encoded_rsa_signature method

--- lib/nemid/crypto.rb
+++ lib/nemid/crypto.rb
@@ -7,19 +7,36 @@ module NemID
   def initialize(certificate, pass)
     certificate = read_file(certificate)
     @pkcs12 = read_pkcs12(certificate, pass)
+    @rsa_instance = rsa_keypair(@pkcs12, pass)
   end
 
   def base64_encoded_der_representation
     Base64.strict_encode64(@pkcs12.certificate.to_der)
   end
 
+  def base64_encoded_rsa_signature(data)
+    Base64.strict_encode64(sign(data))
+  end
+
   private
+  def digest(data)
+    OpenSSL::Digest::SHA256.new(data)
+  end
+
   def read_file(certificate)
     File.read(certificate)
   end
 
   def read_pkcs12(certificate, pass)
     OpenSSL::PKCS12::new(certificate, pass)
+  end
+
+  def rsa_keypair(pkcs12, passphrase)
+    OpenSSL::PKey::RSA.new(pkcs12.key, passphrase)
+  end
+
+  def sign(data)
+    @rsa_instance.sign(OpenSSL::Digest::SHA256.new, data)
   end
  end
 end
