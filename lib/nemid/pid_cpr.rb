4f91c9f0436f005a01830ca36a6c3a536b4e6c5c
ludvig <ludvigrandersen@gmail.com>
2020-08-13 14:22:52 +0200

    adds soap_client method

--- lib/nemid/pid_cpr.rb
+++ lib/nemid/pid_cpr.rb
@@ -10,5 +10,22 @@ module NemID
              @ssl_cert_key_file = ssl_cert_key
         end
 
+
+        private
+        def soap_client
+            pid_service_url = "https://pidws.pp.certifikat.dk/pid_serviceprovider_server/pidws"
+            options = {
+                :wsdl => "#{pid_service_url}?WSDL",
+                :soap_version => 1,
+                :endpoint => pid_service_url,
+                :convert_request_keys_to => :none,
+                :ssl_cert_file => @ssl_cert_file,
+                :ssl_cert_key_file => @ssl_cert_key_file,
+                :headers => { 'SOAPAction' => ''}
+              }
+            client = Savon.client(options)
+            client
+        end
+
     end 
 end
