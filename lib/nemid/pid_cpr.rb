caeb1865af1c57b72c0464d1491c1039f7bd08a4
ludvig <ludvigrandersen@gmail.com>
2020-08-13 15:19:28 +0200

    adds response status code overview to pid_cpr file

--- lib/nemid/pid_cpr.rb
+++ lib/nemid/pid_cpr.rb
@@ -10,6 +10,22 @@ module NemID
             @key = key
         end
 
+=begin
+    STATUS CODE RESPONSES:
+    O = OK (”OK", "OK") 
+    1 = NO_MATCH ("CPR svarer ikke til PID", "CPR does not match PID")
+    2 = NO_PID ("PID eksisterer ikke", "PID doesn't exist") 
+    4 = NO_PID_IN_CERTIFICATE ("PID kunne ikke findes i certifikatet", "No PID in certificate") 
+    8 = NOT_AUTHORIZED_FOR_CPR_LOOKUP ("Der er ikke rettighed til at foretage CPR opslag", "Caller not authorized for CPR lookup") 
+    16 = BRUTE_FORCE_ATTEMPT_DETECTED ("Forsøg på systematisk søgning på CPR", "Brute force attempt detected") 
+    17 = NOT_AUTHORIZED_FOR_SERVICE_LOOKUP ( "Der er ikke rettighed til at foretage opslag på service", "Caller not authorized for service lookup") 
+    4096 = NOT_PID_SERVICE_REQUEST ("Modtaget message ikke pidCprRequest eller pidCprServerStatus", "Non request XML received") 
+    8192 = XML_PARSE_ERROR ("XML pakke kan ikke parses", "Non parsable XML received") 
+    8193 = XML_VERSION_NOT_SUPPORTED ("Version er ikke understøttet", "Version not supported") 
+    8194 = PID_DOES_NOT_MATCH_BASE64_CERTIFICATE ("PID stemmer med ikke med certifikat", "PID does not match certifikat") 
+    8195 = MISSING_CLIENT_CERT ("Klient certifikat ikke præsenteret", "No client certificate presented") 
+    16384 = INTERNAL_ERROR ("Intern DanID fejl", "Internal DanID error")
+=end
         def pid_cpr_match
             @cpr ||= nil
             @soap_client = soap_client
