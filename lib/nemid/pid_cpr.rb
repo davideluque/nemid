4ce98e1875a10f80159639430f432903adb016c1
ludvig <ludvigrandersen@gmail.com>
2020-08-13 14:22:35 +0200

    adds pid_cpr initializer

--- lib/nemid/pid_cpr.rb
+++ lib/nemid/pid_cpr.rb
@@ -3,5 +3,12 @@ require 'savon'
 module NemID
     class PidCpr
 
+        def initialize(spid, pid, ssl_cert, ssl_cert_key)
+             @spid = spid
+             @pid = pid
+             @ssl_cert_file = ssl_cert
+             @ssl_cert_key_file = ssl_cert_key
+        end
+
     end 
 end
