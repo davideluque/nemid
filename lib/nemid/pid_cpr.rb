148144bc95049d128734d699e99e1b7d54fc243f
ludvig <ludvigrandersen@gmail.com>
2020-08-13 15:00:45 +0200

    refactoring of pid_cpr file

--- lib/nemid/pid_cpr.rb
+++ lib/nemid/pid_cpr.rb
@@ -3,11 +3,11 @@ require 'savon'
 module NemID
     class PidCpr
 
-        def initialize(spid, pid, ssl_cert, ssl_cert_key)
-             @spid = spid
-             @pid = pid
-             @ssl_cert_file = ssl_cert
-             @ssl_cert_key_file = ssl_cert_key
+        def initialize(spid, pid, cert, key)
+            @spid = spid
+            @pid = pid
+            @cert = cert
+            @key = key
         end
 
         def pid_cpr_match
@@ -29,7 +29,7 @@ module NemID
             result = response.to_hash[:pid_response][:result][:pid_reply]
             
             if result[:status_code] == "0"
-                result[:cpr]
+                result
             else
                 "PID match failed with status code #{result[:status_code]}"
             end
@@ -43,10 +43,10 @@ module NemID
                 :soap_version => 1,
                 :endpoint => pid_service_url,
                 :convert_request_keys_to => :none,
-                :ssl_cert_file => @ssl_cert_file,
-                :ssl_cert_key_file => @ssl_cert_key_file,
+                :ssl_cert_file => @cert,
+                :ssl_cert_key_file => @key,
                 :headers => { 'SOAPAction' => ''}
-              }
+            }
             client = Savon.client(options)
             client
         end
