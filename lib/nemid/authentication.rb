3dfb9659dea9c31540065aee34aa196c378cfdf8
David Cabeza <cabezadavide@gmail.com>
2020-08-20 00:23:34 -0400

    Add Errors module, APPError class and subclasses

--- lib/nemid/authentication.rb
+++ lib/nemid/authentication.rb
@@ -1,8 +1,8 @@
 module NemID
   module Authentication
-    class ResponseError < StandardError; end
   end
 end
 
+require_relative "authentication/errors"
 require_relative "authentication/params"
 require_relative "authentication/response"
---
+++ lib/nemid/authentication/errors.rb
@@ -0,0 +1,15 @@
+module NemID
+  module Authentication
+    module Errors
+      class ResponseError < StandardError
+        attr_reader :da, :en
+      end
+    end
+  end
+end
+
+require_relative "errors/app"
+#require_relative "errors/auth"
+#require_relative "errors/can"
+#require_relative "errors/capp"
+#require_relative "errors/lock"
