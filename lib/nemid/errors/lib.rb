ab4a4cfadc7dac4f6a154a4c75fe655eedc41f3c
David Cabeza <cabezadavide@gmail.com>
2020-08-20 03:54:10 -0400

    Improve and fix LIB error

--- lib/nemid/errors/lib.rb
+++ lib/nemid/errors/lib.rb
@@ -1,17 +1,17 @@
 module NemID
   module Errors
-    class LIBError
+    class LIBError < ResponseError
       def initialize(msg='')
         super(msg)
       end
     end
 
-    class LIB002Error
-      def initialize(sp='support')
+    class LIB002Error < LIBError
+      def initialize
         @da = "Der er opstået en teknisk fejl på grund af netværksproblemer. Forsøg igen. " \
-        "Kontakt #{sp}, hvis problemet fortsætter."
+        "Kontakt support, hvis problemet fortsætter."
         @en = "A technical error has occurred due to network issues. Please try again. " \
-        "Contact #{sp} if the problem persists"
+        "Contact support if the problem persists"
         super
       end
     end
