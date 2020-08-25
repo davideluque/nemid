68830e4a93a1bf9dc8af9e7da483490a6f2c563f
David Cabeza <cabezadavide@gmail.com>
2020-08-24 22:54:36 -0400

    Removed .rb

--- lib/nemid.rb
+++ lib/nemid.rb
@@ -2,7 +2,7 @@ require 'base64'
 require "nemid/version"
 require "nemid/authentication"
 require "nemid/crypto"
-require "nemid/ocsp.rb"
+require "nemid/ocsp"
 require "nemid/xmldsig"
 
 module NemID
