a118e1aab3c047eec778fe09678886c1d1bf9c58
ludvig <ludvigrandersen@gmail.com>
2020-09-01 10:16:17 +0200

    adds error messages for CAPP classes

--- lib/nemid/errors/capp.rb
+++ lib/nemid/errors/capp.rb
@@ -6,11 +6,29 @@ module NemID
       end
     end
 
-    class CAPP001Error < CAPPError; end
+    class CAPP001Error < CAPPError 
+      def initialize
+        @da = "The code app could not be enrolled due to violation of the app's restricted identity type."
+        @en = "Nøgleappen kunne ikke blive indskrevet på grund af krænkelse af app'ens begrænsede identites type."
+        super
+      end
+    end
 
-    class CAPP002Error < CAPPError; end
+    class CAPP002Error < CAPPError
+      def initialize
+        @da = "Your code app could not be enrolled due to violation of the suspension rules."
+        @en = "Din nøgleapp kunne ikke blive indskrevet på grund af krænkelse af suspensionsreglerne."
+        super
+      end
+    end
     
-    class CAPP003Error < CAPPError; end
+    class CAPP003Error < CAPPError
+      def initialize
+        @da = "Your code app could not be enrolled due to its app id is not in status active."
+        @en = "Din nøgleapp kunne ikke blive indskrevet på grund af, at dens app id ikke er i en aktiv status."
+        super
+      end
+    end
     
     class CAPP004Error < CAPPError
       def initialize
@@ -20,20 +38,68 @@ module NemID
       end
     end
     
-    class CAPP006Error < CAPPError; end
+    class CAPP006Error < CAPPError
+      def initialize
+        @da = "Your code app could not be activated as it could not be found for the user who logged in."
+        @en = "Din nøgleapp kunne ikke blive aktiveret, da den ikke kunne blive fundet for brugeren der loggede ind."
+        super
+      end
+    end
     
-    class CAPP007Error < CAPPError; end
+    class CAPP007Error < CAPPError
+      def initialize
+        @da = "Your code app could not be activated as it did not have the correct status (VALIDATION_MISSING)."
+        @en = "Din nøgleapp kunne ikke blive aktiveret, da den ikke har den korrekte status (VALIDATIONS_MISSING)."
+        super
+      end
+    end
     
-    class CAPP008Error < CAPPError; end
+    class CAPP008Error < CAPPError
+      def initialize
+        @da = "Your code app could not be activated as the waiting period has not expired."
+        @en = "Din nøgleapp kunne ikke blive aktiveret, da venteperioden ikke er uløbet."
+        super
+      end
+    end
     
-    class CAPP009Error < CAPPError; end
+    class CAPP009Error < CAPPError
+      def initialize
+        @da = "Your code app could not be activated as the waiting period has not expired."
+        @en = "Din nøgleapp kunne ikke blive aktiveret, da venteperioden ikke er uløbet."
+        super
+      end
+    end
     
-    class CAPP010Error < CAPPError; end
+    class CAPP010Error < CAPPError
+      def initialize
+        @da = "The reset of your code app pin could not be done as the code app is not in status active."
+        @en = "Nulstilling af din nøgeleapps pin kunne ikke blive fuldført, da din nøgleapp ikke er i en aktiv status."
+        super
+      end
+    end
     
-    class CAPP011Error < CAPPError; end
+    class CAPP011Error < CAPPError
+      def initialize
+        @da = "The validation of the admin action challenge failed."
+        @en = "Valideringen af admin action challenge fejlede."
+        super
+      end
+    end
     
-    class CAPP012Error < CAPPError; end
+    class CAPP012Error < CAPPError
+      def initialize
+        @da = "Your code app could not be enrolled as the code app id does not exist."
+        @en = "Din nøgleapp kunne ikke blive indskrefvet, da dens app id ikke eksisterer."
+        super
+      end
+    end
     
-    class CAPP013Error < CAPPError; end
+    class CAPP013Error < CAPPError
+      def initialize
+        @da = "Your code app could not be enrolled due to incorrect software fingerprint or missing whitelist of software fingerprint"
+        @en = "Din nøgleapp kunne ikke blive indskrevet på grund af forkert software fingeraftryk eller manglende whitelist af software fingeraftryk."
+        super
+      end
+    end
   end
 end
