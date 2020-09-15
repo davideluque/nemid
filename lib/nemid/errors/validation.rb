4401228e05c9723446e2888a765441d1c1c93091
David Cabeza <cabezadavide@gmail.com>
2020-09-14 22:55:37 -0400

    Add msgs and validation to errors module

--- lib/nemid/errors/validation.rb
+++ lib/nemid/errors/validation.rb
@@ -2,12 +2,30 @@ module NemID
   module Errors
     class ResponseValidationError < ResponseError ; end
 
-    class InvalidSignature < ResponseValidationError ; end
+    class InvalidSignature < ResponseValidationError
+      def initialize(
+        msg = "Invalid signature in the XML Document. Data might be altered."
+      )
+      super(msg)
+      end
+    end
 
-    class InvalidCertificateChain < ResponseValidationError ; end
+    class InvalidCertificateChain < ResponseValidationError
+      def initialize(msg = "Invalid certificate chain.")
+        super(msg)
+      end
+    end
 
-    class UserCertificateExpired < ResponseValidationError ; end
+    class UserCertificateExpired < ResponseValidationError
+      def initialize(msg = "User certificate has expired.")
+        super(msg)
+      end
+    end
 
-    class UserCertificateRevoked < ResponseValidationError ; end
+    class UserCertificateRevoked < ResponseValidationError
+      def initialize(msg = "User certificate has been revoked by CA.")
+        super(msg)
+      end
+    end
   end
 end
